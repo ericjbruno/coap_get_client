fb781efa68ff9311d950f99c18fd7db1c735c11b
Eric Bruno <eric@ericbruno.com>
2022-05-10 08:20:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# coap_get_client
---
+++ src/main/java/CoapGetClient.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+
+/**
+ * @author ebruno
+ */
+public class CoapGetClient {
+    
+    static class AsynchListener implements CoapHandler {
+        @Override
+        public void onLoad(CoapResponse response) {
+            String content = response.getResponseText();
+            System.out.println("onLoad: " + content);
+        }
+
+        @Override
+        public void onError() {
+            System.err.println("Error");
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        /*
+        if ( args.length == 0 ) {
+            // display help
+            System.out.println("Usage: " + CoapGetClient.class.getSimpleName() + " URI");
+            System.out.println("  URI: The CoAP URI of the remote resource to GET");
+            System.exit(-1);
+        }
+
+        // Use input URI from command line arguments
+        URI uri = null; // URI parameter of the request
+        try {
+            uri = new URI(args[0]);
+        }
+        catch ( URISyntaxException e ) {
+            System.err.println("Invalid URI: " + e.getMessage());
+            System.exit(-1);
+        }
+        */
+        
+        URI uri = new URI("coap://localhost:5683/helloWorld");
+
+        // synchronous CoAP GET request
+        //
+        CoapClient client = new CoapClient(uri);
+        CoapResponse response = client.get();
+
+        if ( response != null ) {
+            System.out.println(response.getCode());
+            System.out.println(response.getOptions());
+            System.out.println(response.getResponseText());
+            
+            byte[] bytes = response.getPayload();
+
+            System.out.println("\nDETAILED RESPONSE:");
+
+            // access advanced API with access to more details through .advanced()
+            System.out.println(Utils.prettyPrint(response));
+        }
+        else {
+            System.out.println("No response received.");
+        }
+        
+        // asynchronous CoAp GET request
+        //
+        client.get(new CoapHandler() {
+            @Override public void onLoad(CoapResponse response) {
+                String content = response.getResponseText();
+                System.out.println("onLoad: " + content);
+            }
+
+            @Override public void onError() {
+                System.err.println("Error");
+            }
+        });
+
+        // wait for user
+        System.out.println("press enter to continue");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try { br.readLine(); } catch (IOException e) { }
+        
+        AsynchListener asynchListener = new AsynchListener();
+        client.get( asynchListener );
+        // execution continues without waiting...
+        
+        // wait for user
+        System.out.println("press enter to exit");
+        br = new BufferedReader(new InputStreamReader(System.in));
+        try { br.readLine(); } catch (IOException e) { }
+    }
+
+}
